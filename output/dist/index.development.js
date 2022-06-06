e3a5181bec1efc07d12403dd3a658dd0713b4b04
vanquyettran <vanquyettran@hotmail.com>
2022-06-06 17:54:11 +0700

    update VirtualNode add types

--- output/dist/index.development.js
+++ output/dist/index.development.js
@@ -169,51 +169,95 @@ function VirtualNode(type, props, key) {
     // Identification
     // ==============
 
+    /**
+     * @type {string|function}
+     */
     this.type_ = type;
 
-    // Convert to string to avoid conflict with slot
+    /**
+     * @type {string|null}
+     */
     this.key_ = key;
 
+    /**
+     * @type {number|null}
+     */
     this.slot_ = null;
 
     // Props and hooks
     // ===============
 
+    /**
+     * @type {{}}
+     */
     this.props_ = props;
 
+    /**
+     * @type {RefHook|null}
+     */
     this.refHook_ = null;
     
+    /**
+     * @type {StateHook|null}
+     */
     this.stateHook_ = null;
 
+    /**
+     * @type {EffectHook|null}
+     */
     this.effectHook_ = null;
     
     // Output native node and relates
     // ==============================
     
+    /**
+     * @type {Node}
+     */
     this.nativeNode_ = null;
 
+    /**
+     * @type {string|null}
+     */
     this.namespace_ = null;
 
     // Linked-list pointers
     // ====================
 
+    /**
+     * @type {VirtualNode|null}
+     */
     this.parent_ = null;
     
+    /**
+     * @type {VirtualNode|null}
+     */
     this.child_ = null;
 
+    /**
+     * @type {VirtualNode|null}
+     */
     this.sibling_ = null;
 
     // Temp props
     // ==========
     
     // The previous version of this node
+    /**
+     * @type {VirtualNode|null}
+     */
     this.alternative_ = null;
 
     // The children (and their subtrees, of course) are marked to be deleted
+    /**
+     * @type {VirtualNode[]}
+     */
     this.deletions_ = null;
  
     // In the commit phase, the new child will be inserted
     // after the last inserted/updated child
+    /**
+     * @type {Node|null}
+     */
     this.lastManipulatedClient_ = null;
 }
 
