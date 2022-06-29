1e59332befd1e1bc22c4872694679e51357db5cf
Tran Van Quyet <vanquyettran@hotmail.com>
2022-06-29 18:19:14 +0700

    playground: add usecase in React

---
+++ playground/src/usecases/stateContext2_react.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from "react";
+import {render} from 'react-dom';
+
+render(<A />, document.getElementById("root"));
+
+function A() {
+    const [shows, setShows] = useState(0);
+
+    console.log('shows in render A', shows);
+
+    useEffect(() => {
+        console.log('click in effect A');
+        document.querySelector(".btn-set-state").click();
+    }, []);
+
+    return (
+        <div>
+            <button
+                class="btn-set-state"
+                onClick={() => {
+                    setShows(1);
+                }}
+            >
+                Set State
+            </button>
+            <B shows={shows} />
+        </div>
+    );
+}
+
+function B({ shows }) {
+    const [clicks, setClicks] = useState(0);
+
+    console.log('shows in render B', shows);
+
+    useEffect(() => {
+        setClicks((c) => c + 1);
+    }, []);
+
+    useEffect(() => {
+        console.log('shows in effect B', shows);
+    }, [shows]);
+
+    return `Shows ${shows} ; Clicks ${clicks}`;
+}
